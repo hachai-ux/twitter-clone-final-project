370f45f8fe8e66e04ea16b13e82bda170c5d7fc5
Hac Hai Pham <hpham.ux@gmail.com>
2021-10-28 14:33:19 +0200

    Add delete button for status

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,9 +1,59 @@
+import { useState, useEffect } from 'react';
+import { deleteDoc } from "firebase/firestore";
+
 const Status = (props) => {
+
+    const [dropdownStatus, setDropdownStatus] = useState(false);
+
+         
+    const deleteStatus = async () => {
+        console.log(props.doc.data());
+        await deleteDoc(props.doc);
+        //props.changeStatusSubimport { deleteDoc } from "firebase/firestore";mitted(true);
+}
+
+
+const showDropdown = () => {
+    setDropdownStatus(true);
+}
+   
+    
+    window.onclick = function (e) {
+if (!e.target.matches('.dropbtn')) {
+    setDropdownStatus(false);
+}
+};
+
+
+
+
+
+    
+    
+    const Dropdown = () => {
+        if (dropdownStatus === true) {
+            return (
+                 <div id="myDropdown" className="dropdown-content">
+                        <button onClick={deleteStatus}>Delete</button>
+                      
+                    </div>
+
+            )
+        }
+        else return null;
+    }
+
     return (
         <div>
             <div>{props.doc.data().name}</div>
             <div>{props.doc.data().status}</div>
             <div>{props.doc.data().timestamp.toDate().toString()}</div>
+             <div className="dropdown">
+                <button onClick={showDropdown} className="dropbtn">...</button>
+                <Dropdown />
+                  
+                </div>
+            
          
        
            
--- src/components/UserStatuses.js
+++ src/components/UserStatuses.js
@@ -1,24 +1,27 @@
 import { useEffect, useState } from 'react';
-import { collection, query, getDocs, where } from 'firebase/firestore';
+import { collection, query, getDocs, where, orderBy } from 'firebase/firestore';
 import Status from './Status.js';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
+
 
 const UserStatuses = (props) => {
 
     const [statuses, setStatuses] = useState(null);
+
+
 
     useEffect(() => {
 
         
         const getStatuses = async () => {
             console.log('Hey');
-            const q = query(collection(props.db, "Tweets",props.user.uid,"Statuses"), where("name", "==", props.user.uid));
+            const q = query(collection(props.db, "Tweets",props.user.uid,"Statuses"), where("name", "==", props.user.uid), orderBy("timestamp", 'desc'));
 
             const querySnapshot = await getDocs(q);
             setStatuses(querySnapshot.docs.map((doc) => {
                 // doc.data() is never undefined for query doc snapshots
                         console.log(doc.id, " => ", doc.data());
-                return <li key={nanoid()}><Status doc={doc}/></li>
+                return <li key={nanoid()}><Status doc={doc} changeStatusSubmitted={props.changeStatusSubmitted}/></li>
                 
             }));
 
