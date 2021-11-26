804f343b2dd6146ab497684d0d1af91f073ccb3f
Hac Hai Pham <hpham.ux@gmail.com>
2021-11-26 16:52:08 +0100

    Add follow button to who to follow users

--- src/components/ProfileSuggestions.js
+++ src/components/ProfileSuggestions.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { orderBy, startAt, limit, query, collectionGroup, collection, where, getDocs } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
+import Follow from './Follow';
 
 const ProfileSuggestions = (props) => {
 
@@ -87,7 +88,10 @@ const [profileDocs, setProfileDocs] = useState([]);
             if (doc !== null) {
                 const usernamePath = `/${doc.data().username}`;
                 return <li>
-                    <Link to={usernamePath}>{doc.data().username}</Link></li>
+                    <Link to={usernamePath}>{doc.data().username}</Link>
+                    <Follow db={props.db} profileSnap = {doc} profilename={doc.data().username} username={props.username} user= {props.user} />
+                </li>
+            
             }
             else return null;
         }
