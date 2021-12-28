f60bb494f25116c66e40efca3f699b8216f5b1fa
Hac Hai Pham <hpham.ux@gmail.com>
2021-12-28 15:31:18 +0100

    Add landing page styling

--- src/sites/LandingPage.js
+++ src/sites/LandingPage.js
@@ -1,4 +1,4 @@
-
+import logo from "../content/Twitter-Emblem.png";
 
 const LandingPage = (props) => {
 
@@ -6,12 +6,18 @@ const LandingPage = (props) => {
 
 
     return (
+
         <div className="landing-page">
-            <img src="" alt=""></img>
+            <div className="landing-background">
+                <img src={logo} alt="Twitter Clone Logo"></img>
+            </div>
+            <div className="landing-content">
             <h1>Join Twitter's Alter-Ego</h1>
-            <h2>Register for Twitter Clone</h2>
-            <button>Signup</button>
-            <button>Login</button>
+            <h2>Register for Twitter Clone.</h2>
+            <button className="btn signup-btn"><span>Signup</span></button>
+            <button className="btn login-btn"><span>Login</span></button>
+            </div>
+            
         </div>
         
     );
