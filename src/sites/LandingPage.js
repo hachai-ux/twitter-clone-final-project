edb0ba9a8405adb60e3c48eb574f55e8d2b3e22c
Hac Hai Pham <hpham.ux@gmail.com>
2021-12-28 17:57:51 +0100

    Conditionally show forms

--- src/sites/LandingPage.js
+++ src/sites/LandingPage.js
@@ -1,8 +1,32 @@
 import logo from "../content/Twitter-Emblem.png";
+import { useState } from 'react';
+import SignUp from "./SignUp";
+import Login from "./Login";
 
 const LandingPage = (props) => {
 
- 
+    const [signupIsOpen, setSignupIsOpen] = useState(false);
+    const [loginIsOpen, setLoginIsOpen] = useState(false);
+
+    const openSignup = () => {
+        setSignupIsOpen(true);
+    }
+
+    const openLogin = () => {
+        setLoginIsOpen(true);
+    }
+
+    
+    let form;
+    if (signupIsOpen === true && loginIsOpen === false) {
+    form = <SignUp db={props.db} />
+    }
+    else if (signupIsOpen === false && loginIsOpen === true) {
+       form = <Login db={props.db} /> 
+    }
+    else form = null;
+    
+    
 
 
     return (
@@ -14,9 +38,10 @@ const LandingPage = (props) => {
             <div className="landing-content">
             <h1>Join Twitter's Alter-Ego</h1>
             <h2>Register for Twitter Clone.</h2>
-            <button className="btn signup-btn"><span>Signup</span></button>
-            <button className="btn login-btn"><span>Login</span></button>
+                <button onClick={openSignup} className="btn signup-btn"><span>Signup</span></button>
+                <button onClick={openLogin} className="btn login-btn"><span>Login</span></button>
             </div>
+            {form}
             
         </div>
         
